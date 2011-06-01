375d1900dbb6c6e7effe75f5ccd962b24a211419
Kaikz <xkaikz@gmail.com>
2011-06-01 17:40:37 +1200

    removed non-working things, added proper, working things

--- TerraCmd.cs
+++ TerraCmd.cs
@@ -6,15 +6,24 @@ using System.Configuration;
 using System.Collections.Specialized;
 using System.Collections;
 using System.IO;
+using System.Threading;
+using Microsoft.Xna.Framework;
 
 namespace Terraria
 {
     class TerraCmd : Plugin
     {
-
+        // Settings
+        public Dictionary<string, string> settings;
+        public string settingsPath = "terracmd.txt";
+        // OP config
         public string opTag;
-        public string opTagColor;
-        public ArrayList rules = new ArrayList();
+        public int opR, opG, opB;
+        // motd config
+        public int motdR, motdG, motdB;
+        // Save config
+        public int saveInterval = 600000;
+        private Thread saveThread; 
 
         public override void Initialize()
         {
@@ -25,23 +34,72 @@ namespace Terraria
 
             this.registerHook(Hook.PLAYER_COMMAND);
             this.registerHook(Hook.PLAYER_CHAT);
+            this.registerHook(Hook.PLAYER_JOIN);
 
-            opTag = ConfigurationManager.AppSettings.Get("opTag");
-            opTagColor = ConfigurationManager.AppSettings.Get("opTagColor");
+            loadSettings();
 
-            string loaded = "loaded!";
-            Console.WriteLine(string.Concat(new object[] { ConsoleColor.Red, "[TerraCmd] ", loaded }));
+            Console.WriteLine("[TerraCmd] loaded!");
+
+            this.saveThread = new Thread(new ThreadStart(this.saveMap));
+            this.saveThread.Start();
         }
 
         public override void Unload()
         {
-            string unloaded = "unloaded!";
-            Console.WriteLine(string.Concat(new object[] { ConsoleColor.Red, "[TerraCmd] ", unloaded }));
+            Console.WriteLine("[TerraCmd] unloaded!");
         }
 
         public bool equalsIgnoreCase(string a, string b)
         {
             return a.Equals(b, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public void motd(Player p)
+        {
+            string msg = this.settings["motd"];
+            msg = msg.Replace("[player]", p.name).Replace("[server]", Main.getIP ?? "");
+            p.sendMessage(msg, motdR, motdG, motdB);
+        }
+
+        public void saveMap()
+        {
+            while (true)
+            {
+                Thread.Sleep(this.saveInterval);
+                NetMessage.broadcastMessage("Saving world... this might lag for a minute!");
+                WorldGen.saveWorld(false);
+            }
+        }
+
+ 
+
+
+        public void loadSettings()
+        {
+            this.settings.Clear();
+            if (!File.Exists(settingsPath))
+            {
+                TextWriter writer = new StreamWriter(settingsPath);
+                writer.WriteLine("opTag=[OP]");
+                writer.WriteLine("opR=255");
+                writer.WriteLine("opG=0");
+                writer.WriteLine("opB=0");
+                writer.WriteLine("motd=Welcome to [server], [player]!");
+                writer.WriteLine("motdR=51");
+                writer.WriteLine("motdG=255");
+                writer.WriteLine("motdB=0");
+                writer.Close();
+            }
+            foreach (string str2 in File.ReadAllLines(settingsPath))
+            {
+                this.settings.Add(str2.Split(new char[] { '=' })[0], str2.Split(new char[] { '=' })[1]);
+            }
+
+            // OP Settings
+            opTag = this.settings["opTag"];
+            opR = int.Parse(this.settings["opR"]);
+            opG = int.Parse(this.settings["opG"]);
+            opB = int.Parse(this.settings["opB"]);
         }
 
         public Player getPlayerFromString(string p)
@@ -80,62 +138,21 @@ namespace Terraria
                 ev.getPlayer().sendMessage("Players Online: " + str + ".", 0xff, 240, 20);
                 ev.setState(true);
             }
-            else if (cmd[0].ToLower() == "/rules")
+            else if (cmd[0].ToLower() == "/spawnnpc" && ev.getPlayer().isOP)
             {
-                loadRules(ev.getPlayer());
-                foreach (string stringRules in this.rules)
+                try
                 {
-                    player.sendMessage(stringRules, 51, 255, 0);
+                    NPCCode code = (NPCCode)Enum.Parse(typeof(NPCCode), cmd[1], true);
+                    NPC.NewNPC(((int)ev.getPlayer().position.X) + 5, (int)ev.getPlayer().position.Y, (int)code, 0);
+                    ev.getPlayer().sendMessage("Spawned " + cmd[1], 0x33, 0xcc, 0);
+                }
+                catch (Exception)
+                {
+                    ev.getPlayer().sendMessage("Invalid NPC", 0xff, 0, 0);
                 }
                 ev.setState(true);
-            }
-            else if (cmd[0] == "/heal")
-            {
-                ev.getPlayer().statLife = ev.getPlayer().statLifeMax;
-                ev.getPlayer().statMana = ev.getPlayer().statManaMax;
-                ev.getPlayer().sendMessage("You have just been healed!", 51, 255, 0);
-                NetMessage.syncPlayers();
-                ev.setState(true);
-            }
-            else if (cmd[0] == "/kill" && cmd[1] != null)
-            {
-                Player killPlayer = getPlayerFromString(cmd[1]);
-                if (killPlayer.active)
-                {
-                    killPlayer.Hurt(99999, 0, true, false);
-                    NetMessage.syncPlayers();
-                    ev.setState(true);
-                }
-                else
-                {
-                    ev.getPlayer().sendMessage("Player: " + killPlayer + " isn't online!", 51, 255, 0);
-                    ev.setState(true);
-                }
-            }
-            else if (cmd[0] == "/god" && ev.getPlayer().isOP)
-            {
-                if (cmd[1] == "on")
-                {
-                    ev.getPlayer().statLife = 99999;
-                    ev.getPlayer().statMana = 99999;
-                    ev.getPlayer().noFallDmg = true;
-                    ev.getPlayer().noKnockback = true;
-                    ev.getPlayer().breath = 99999;
-                    NetMessage.syncPlayers();
-                    ev.getPlayer().sendMessage("God mode activated!", 51, 255, 0);
-                    ev.setState(true);
-                }
-                else if (cmd[1] == "off")
-                {
-                    ev.getPlayer().statLife = ev.getPlayer().statLifeMax;
-                    ev.getPlayer().statMana = ev.getPlayer().statManaMax;
-                    ev.getPlayer().noFallDmg = false;
-                    ev.getPlayer().noKnockback = false;
-                    ev.getPlayer().breath = ev.getPlayer().breathMax;
-                    NetMessage.syncPlayers();
-                    ev.getPlayer().sendMessage("God mode deactivated!", 51, 255, 0);
-                    ev.setState(true);
-                }
+                return;
+
             }
         }
 
@@ -146,49 +163,71 @@ namespace Terraria
 
             if (player.isOP)
             {
-                switch (opTagColor)
+                foreach (Player player2 in Main.player)
                 {
-                    case "Red":
-                        NetMessage.broadcastMessage(string.Concat(new object[] { "<", ConsoleColor.Red, opTag, player.name, "> ", str }));
-                        break;
-                    case "Blue":
-                        NetMessage.broadcastMessage(string.Concat(new object[] { "<", ConsoleColor.Blue, opTag, player.name, "> ", str }));
-                        break;
-                    case "Green":
-                        NetMessage.broadcastMessage(string.Concat(new object[] { "<", ConsoleColor.Green, opTag, player.name, "> ", str }));
-                        break;
-                    case "Cyan":
-                        NetMessage.broadcastMessage(string.Concat(new object[] { "<", ConsoleColor.Cyan, opTag, player.name, "> ", str }));
-                        break;
-                    case "Magenta":
-                        NetMessage.broadcastMessage(string.Concat(new object[] { "<", ConsoleColor.Magenta, opTag, player.name, "> ", str }));
-                        break;
-                    case "Yellow":
-                        NetMessage.broadcastMessage(string.Concat(new object[] { "<", ConsoleColor.Yellow, opTag, player.name, "> ", str }));
-                        break;
-                    default:
-                        NetMessage.broadcastMessage(string.Concat(new object[] { "<", ConsoleColor.Red, opTag, player.name, "> ", str }));
-                        break;
+                    if (player2.name.Length > 0)
+                    {
+                        player2.sendMessage("<" + opTag + player.name + "> " + str, opR, opG, opB);
+                    }
                 }
+                ev.setState(false);
+            } else
+            {
+                base.onPlayerChat(ev);
+                ev.setState(false);
             }
         }
 
-        public void loadRules(Player player)
+        public override void onPlayerJoin(PlayerEvent ev)
         {
-            string rulesFile = "rules.txt";
-            string rulesOutput;
-            if (!File.Exists(rulesFile))
-            {
-                player.sendMessage("Rules file not found! Create a rules.txt file!", 255, 0, 0);
-            }
-            else
-            {
-                TextReader reader = new StreamReader(rulesFile);
-                while ((rulesOutput = reader.ReadLine()) != null)
-                {
-                    this.rules.Add(rulesOutput);
-                }
-            }
+            this.motd(ev.getPlayer());
         }
+    }
+
+    public enum NPCCode
+    {
+        AngryBones = 0x1f,
+        ArmsDealer = 0x13,
+        BlueSlime = 1,
+        BoneSerpentBody = 40,
+        BoneSerpentHead = 0x27,
+        BoneSerpentTail = 0x29,
+        BuringSkull = 0x22,
+        BurningSphere = 0x19,
+        ChaosBall = 30,
+        DarkCaster = 0x20,
+        Demolitionist = 0x26,
+        DemonEye = 2,
+        DevourerBody = 8,
+        DevourerHead = 7,
+        DevourerTail = 9,
+        Dryad = 20,
+        EaterOfSouls = 6,
+        EaterOfWorldaTail = 15,
+        EaterOfWorldsBody = 14,
+        EaterOfWorldsHead = 13,
+        EyeOfCthulhu = 4,
+        FireImp = 0x18,
+        GiantWormBody = 11,
+        GiantWormHead = 10,
+        GiantWormTail = 12,
+        GoblinPeon = 0x1a,
+        GoblinSorcerer = 0x1d,
+        GoblinThief = 0x1b,
+        GoblinWarrior = 0x1c,
+        Guide = 0x16,
+        Hornet = 0x2a,
+        ManEater = 0x2b,
+        Merchant = 0x11,
+        MeteorHead = 0x17,
+        MotherSlime = 0x10,
+        Nurse = 0x12,
+        OldMan = 0x25,
+        ServantOfCthulu = 5,
+        Skeleton = 0x15,
+        SkeletronHand = 0x24,
+        SkeletronHead = 0x23,
+        WaterSphere = 0x21,
+        Zombie = 3
     }
 }
